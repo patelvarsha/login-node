2c352a4979d64751ea6d165008bc5b0e32943e76
patelvarsha <patelvarsha59848@gmail.com>
2023-04-13 12:03:25 +0530

    with node API

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "student",
+});
+
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "INSERT INTO user(username,password) VALUES (?,?)",
+    [username, password],
+    (err, result) => {
+      console.log(err);
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * FROM user WHERE username = ? AND password = ?",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({ message: "Wrong username and password!" });
+      }
+    }
+  );
+});
+
+app.listen(3005, () => {
+  console.log("running server!");
+});
